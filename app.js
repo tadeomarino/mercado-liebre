356d29e6d652544a36c70f730350659dcc590d7f
tadeomarino <tadeomarinoa1@gmail.com>
2022-04-10 18:41:45 -0300

    Mercado-Liebre-Demo Terminado

--- app.js
+++ app.js
@@ -15,3 +15,11 @@ app.listen(PORT, () => {
 app.get("/",(req, res) => {
     res.sendFile(path.join(__dirname, "/views/home.html"));
 });
+
+app.get("/register",(req, res) => {
+    res.sendFile(path.join(__dirname, "/views/register.html"));
+});
+
+app.get("/login",(req, res) => {
+    res.sendFile(path.join(__dirname, "/views/login.html"));
+});
